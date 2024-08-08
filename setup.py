91caa4b30e9a88c24bc55cec8433df37a904a46c
Ranjeet-Swami <ranjitswami406@gmail.com>
2024-08-09 02:37:31 +0530

    created setup file day 1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+#from typing import List
+
+
+setup (
+name="sensor",
+version="0.0.1",
+author="ranjeet",
+author_email="ranjitswami406@gmail.com",
+packages=find_packages(),
+install_requires = ["pymongo"]
+)
